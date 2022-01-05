1b533f0cc1f51aa60080df693eb9094a659ecc44
SarumanIsAlive <90073141+SarumanIsAlive@users.noreply.github.com>
2022-01-05 09:27:43 +0200

    fixed changes. deleted redundant lines, added constructor, naming.

--- src/main/java/cinema/util/HashUtil.java
+++ src/main/java/cinema/util/HashUtil.java
@@ -7,19 +7,22 @@ import java.security.SecureRandom;
 public class HashUtil {
     public static final String HASH_ALGORITHM = "SHA-512";
 
+    public HashUtil() {
+    }
+
     public static String hashPassword(String password, byte[] salt) {
-        StringBuilder hashedPwd = new StringBuilder();
+        StringBuilder hashedPassword = new StringBuilder();
         try {
             MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM);
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
-            for (byte b : digest) {
-                hashedPwd.append(String.format("%02x", b));
+            for (byte symbol : digest) {
+                hashedPassword.append(String.format("%02x", symbol));
             }
-            return hashedPwd.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Can`t hash password!", e);
+        } catch (NoSuchAlgorithmException exception) {
+            throw new IllegalStateException("Can't create hash using SHA-512 algorithm", exception);
         }
+        return hashedPassword.toString();
     }
 
     public static byte[] getSalt() {
